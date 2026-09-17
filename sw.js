27fd6acfd34524f5e4a756b477386193fb9b6099
leonardogda-gif <leonardo.gda@gmail.com>
2026-09-17 00:58:37 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "diario-estudos-v13.0";
+const CACHE_NAME = "diario-estudos-v13.1";
 const APP_SHELL = ["./", "./index.html", "./version.json"];
 
 self.addEventListener("install", event => {
