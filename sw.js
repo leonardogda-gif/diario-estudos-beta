988821dc2dc57bd0f6c25872b314eec1d3a1d3d9
leonardogda-gif <leonardo.gda@gmail.com>
2026-09-20 09:52:11 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "diario-estudos-v13.13";
-const APP_SHELL = ["./", "./index.html", "./version.json", "./manifest.json", "./icons/icon-192-oficial-v3.png", "./icons/icon-512-oficial-v3.png", "./icons/icon-192-maskable-oficial-v3.png", "./icons/icon-512-maskable-oficial-v3.png", "./icons/favicon-32-oficial-v3.png", "./icons/favicon-48-oficial-v3.png", "./icons/favicon-oficial-v2.svg"];
+const CACHE_NAME = "diario-estudos-v13.15";
+const APP_SHELL = ["./", "./index.html", "./version.json", "./manifest.json", "/icons/icon-44-oficial-v4.png", "/icons/icon-48-oficial-v4.png", "/icons/icon-128-oficial-v4.png", "/icons/icon-150-oficial-v4.png", "/icons/icon-192-oficial-v4.png", "/icons/icon-256-oficial-v4.png", "/icons/icon-310-oficial-v4.png", "/icons/icon-512-oficial-v4.png", "/icons/icon-192-maskable-oficial-v4.png", "/icons/icon-512-maskable-oficial-v4.png", "/icons/favicon-32-oficial-v4.png", "/icons/favicon-48-oficial-v4.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
